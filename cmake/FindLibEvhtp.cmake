71f450fc0ec8e1eaf507f7098b9d947d5e8cb63a
DGrudinin <dmitry.grudinin@gmail.com>
2026-02-04 18:31:40 +0300

    Update CMake find scripts to use consistent naming conventions
    - Changed the package names in find_package_handle_standard_args from uppercase to camel case for LibEvent, LibEvhtp, and LibZ to ensure consistency across CMake configuration files.

--- cmake/FindLibEvhtp.cmake
+++ cmake/FindLibEvhtp.cmake
@@ -13,7 +13,7 @@ include (FindPackageHandleStandardArgs)
 set (LIBEVHTP_INCLUDE_DIRS ${LIBEVHTP_INCLUDE_DIR})
 set (LIBEVHTP_LIBRARIES ${LIBEVHTP_LIBRARY})
 
-find_package_handle_standard_args (LIBEVHTP DEFAULT_MSG LIBEVHTP_LIBRARIES LIBEVHTP_INCLUDE_DIR)
+find_package_handle_standard_args (LibEvhtp DEFAULT_MSG LIBEVHTP_LIBRARIES LIBEVHTP_INCLUDE_DIR)
 mark_as_advanced(LIBEVHTP_INCLUDE_DIRS LIBEVHTP_LIBRARIES)
 
 # Check if WebSocket support is available (evhtp_ws.h exists)
--- cmake/FindLibZ.cmake
+++ cmake/FindLibZ.cmake
@@ -13,5 +13,5 @@ include (FindPackageHandleStandardArgs)
 set (LIBZ_INCLUDE_DIRS ${LIBZ_INCLUDE_DIR})
 set (LIBZ_LIBRARIES ${LIBZ_LIBRARY})
 
-find_package_handle_standard_args (LIBZ DEFAULT_MSG LIBZ_LIBRARIES LIBZ_INCLUDE_DIR)
+find_package_handle_standard_args (LibZ DEFAULT_MSG LIBZ_LIBRARIES LIBZ_INCLUDE_DIR)
 mark_as_advanced(LIBZ_INCLUDE_DIRS LIBZ_LIBRARIES)
